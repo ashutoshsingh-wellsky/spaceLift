1403d197fba5a4a74607119e242110473af277ee
ashutoshsingh-wellsky <ashutosh.singh@wellsky.com>
2025-07-21 10:43:11 -0500

    Remove outdated before-init hook and clean up Terraform state and configuration files

--- TerraForm/backend.tf
+++ TerraForm/backend.tf
@@ -2,7 +2,5 @@ terraform {
   backend "gcs" {
     bucket  = "hhh-attachments-store-dev-8898"                      # <-- Use your actual bucket name
     prefix  = "spacelift/temp-bucket"                # <-- Path within bucket
-    project = "temp-ksaj9na8-wsky"                   # <-- Your GCP project ID
-    location = "US-CENTRAL1"
   }
 }
