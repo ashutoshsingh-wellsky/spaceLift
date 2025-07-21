103487e61a8710ff46088ad2952c33e30088200e
ashutoshsingh-wellsky <ashutosh.singh@wellsky.com>
2025-07-21 12:15:16 -0500

    Restore Terraform backend configuration template for GCS

---
+++ TerraForm/infra/dev/backend-BK.tf
@@ -0,0 +1,6 @@
+# terraform {
+#   backend "gcs" {
+#     bucket  = "hhh-attachments-store-dev-8898"                      # <-- Use your actual bucket name
+#     prefix  = "spacelift/temp-bucket"                # <-- Path within bucket
+#   }
+# }
--- TerraForm/infra/dev/backend.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-  backend "gcs" {
-    bucket  = "hhh-attachments-store-dev-8898"                      # <-- Use your actual bucket name
-    prefix  = "spacelift/temp-bucket"                # <-- Path within bucket
-  }
-}
